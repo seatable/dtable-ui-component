ef8b139f260428db1e37aa474a1af78a67890bad
shanshuirenjia <978987373@qq.com>
2020-03-11 11:03:30 +0800

    add document test

---
+++ src/components/custom-button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CustomButton  extends React.Component {
+
+  render() {
+    return (
+      <div>{'seatable component doc intruductio'}</div>
+    );
+  }
+}
+
+
+export default CustomButton;
