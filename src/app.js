7dc326a4af23e723213374cceec639500d2045a5
shanshuirenjia <978987373@qq.com>
2020-03-12 15:16:37 +0800

    add component

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import { CollaboratorFormatter } from './components/cell-formatter';
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">{'seatable ui component test'}</header>
-    </div>
-  );
+class App extends React.Component {
+
+
+  onDeleteCollaborator = () => {
+    //
+  }
+
+  render() {
+    let collaborators = [
+      {
+        name: '小强',
+        email: '214402@qq.com',
+        avatar_url: '你好饿',
+        contact_email: 'adbd'
+      }
+    ];
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1 className="text-center">{'seatable ui component test'}</h1>
+        </header>
+        <div className="app-body">
+          <div>
+            <h2>collaborators测试</h2>
+            <div>单个collaborators测试</div>
+            <CollaboratorFormatter value={'123'} collaborators={collaborators} />
+            <div>多个collaborators测试</div>
+            <CollaboratorFormatter value={['123', '4567']} collaborators={collaborators} />
+            <div>带删除按钮测试</div>
+            <CollaboratorFormatter 
+              value={['123', '4567']} 
+              collaborators={collaborators} 
+              enableDeleteCollaborator={true} 
+              onDeleteCollaborator={this.onDeleteCollaborator}
+            />
+          </div>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
