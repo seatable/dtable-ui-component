d8abc5bc54dfd2818753b3f6f638219f72396cac
shanshuirenjia <978987373@qq.com>
2020-03-13 18:34:17 +0800

    change chinese to english

--- stories/utils/welcome.js
+++ stories/utils/welcome.js
@@ -5,9 +5,9 @@ class Welcome extends React.Component {
   render() {
     return (
       <div className="intro-wrapper">
-        <h1 className="text-center">欢迎使用dtable-ui-component</h1>
+        <h1 className="text-center">dtable-ui-component manual</h1>
         <div>
-          <p>组件库使用简介</p>
+          <p>Component library introduction</p>
         </div>
       </div>
     );
