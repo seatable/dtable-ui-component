4521db629a6bc851fcf22b058bc25b7db6e9f1c5
shanshuirenjia <978987373@qq.com>
2020-03-14 11:26:23 +0800

    add mutiple-select formatter

--- src/components/cell-formatter/index.js
+++ src/components/cell-formatter/index.js
@@ -2,3 +2,4 @@ import '../../assets/css/cell-formatter.css';
 
 export { default as CollaboratorFormatter } from './collaborator-formatter';
 export { default as SingleSelectFormatter } from './single-select-formatter';
+export { default as MultipleSelectFormatter } from './multiple-select-formatter';
