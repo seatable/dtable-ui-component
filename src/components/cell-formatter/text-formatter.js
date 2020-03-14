4aadf76354a7bc85c3647eadca503610cfca458f
shanshuirenjia <978987373@qq.com>
2020-03-14 11:47:57 +0800

    add text-formatter

---
+++ src/components/cell-formatter/text-formatter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'astro-classname';
+
+const propTypes = {
+  value: PropTypes.string,
+  containerClassName: PropTypes.string,
+};
+
+class TextFormatter extends React.Component {
+
+  render() {
+    const { containerClassName, value } = this.props;
+    let classname = cn('cell-formatter-container single-select-formatter', containerClassName);
+    return (
+      <div className={classname}>{value}</div>
+    );
+  }
+}
+
+TextFormatter.propTypes = propTypes;
+
+export default TextFormatter;
---
+++ stories/components/cell-formatter/4-text-formatter.stories.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import ShowCode from '../../utils/show-code';
+import { TextFormatter } from '../../../src/components/cell-formatter';
+
+import '../../css/cell-formatter.css';
+
+
+const info = {
+  text: '<h1>API</h1>',
+  inline: true,
+  source: false,
+  propTablesExclude: [ShowCode],
+  styles: {
+    header: {
+      h1: {
+        'marginBottom': '8px'
+      }
+    }
+  }
+};
+
+let value1 = 'How do you like to go up in a swing, 你喜欢荡一趟秋千，'
+let value2 = 'Up in the air so blue? 置身于蓝蓝的晴空吗？'
+let value3 = '清晨入古寺，初日照高林。曲径通幽处，禅房花木深。'
+let value4 = '山光悦鸟性，潭影空人心。万籁此都寂，但余钟磬音。'
+
+storiesOf('CELLS|text-formatter', module)
+  .addDecorator(withInfo)
+  .add('text component', () => (
+    <div>
+      <h1>Sample display</h1>
+      <ShowCode>
+        <TextFormatter value={value1} containerClassName={'text-container'} />
+      </ShowCode>
+      <ShowCode>
+        <TextFormatter value={value2} containerClassName={'text-container'} />
+      </ShowCode>
+      <ShowCode>
+        <TextFormatter value={value3} />
+      </ShowCode>
+      <ShowCode>
+        <TextFormatter value={value4} />
+      </ShowCode>
+    </div>
+  ), {info})
+
+
+
+
+
+
