dff8d15e3ed789ba14de73c176aea3314be22466
shanshuirenjia <978987373@qq.com>
2020-04-10 19:57:28 +0800

    export component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
+export { setLocale } from './lang';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export { 
+  formatDateToString,
+  formatNumberToString,
+  fromatStringToNumber,
+  formatNumberString,
+} from './utils/value-format-utils';
 
+export { default as Loading } from './components/loading';
+
+export {
+  TextFormatter,
+  NumberFormatter,
+  CheckboxFormatter,
+  DateFormatter,
+  SingleSelectFormatter,
+  MultipleSelectFormatter,
+  CollaboratorFormatter,
+  ImageFormatter,
+  FileFormatter,
+  LongTextFormatter,
+  GeolocationFormatter
+} from './components/cell-formatter';
+
+export {
+  TextEditor,
+  NumberEditor,
+  CheckboxEditor,
+  SingleSelectEdtior
+} from './components/cell-editor';
