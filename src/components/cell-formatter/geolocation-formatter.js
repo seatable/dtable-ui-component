59a2ad4b9b261ad9f766d426e738bf472c4bbdf7
shanshuirenjia <978987373@qq.com>
2020-04-10 19:48:54 +0800

    add geoglocation formatter

---
+++ src/components/cell-formatter/geolocation-formatter.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'astro-classname';
+
+const propTypes = {
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  containerClassName: PropTypes.string,
+};
+
+class GeolocationFormatter extends React.Component {
+
+  static defaultProps = {
+    value: {
+      province: '',
+      city: '',
+      district: '',
+      detail: '',
+    },
+    containerClassName: ''
+  }
+
+  render() {
+    let { value, containerClassName } = this.props;
+    let classname = cn('cell-formatter-container geolocation-formatter', containerClassName);
+
+    if (typeof value !== 'object') {
+      return null;
+    }
+
+    return (
+      <div className={classname}>
+        {`${value.province || ''} ${value.city || ''} ${value.district || ''} ${value.detail || ''}`}
+      </div>
+    );
+  }
+}
+
+GeolocationFormatter.propTypes = propTypes;
+
+export default GeolocationFormatter;
---
+++ stories/components/cell-formatter/11-geolocation-formatter.stories.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import ShowCode from '../../utils/show-code';
+import { GeolocationFormatter } from '../../../src/components/cell-formatter';
+
+const info = {
+  text: '<h1>API</h1>',
+  inline: true,
+  source: false,
+  propTablesExclude: [ShowCode],
+  styles: {
+    header: {
+      h1: {
+        'marginBottom': '8px'
+      }
+    }
+  }
+};
+
+const value1_1 = '';
+
+const value1_2 = {
+  province: '河南',
+  city: '郑州',
+  district: '二七',
+  detail: '中原路',
+}
+
+storiesOf('CELLS|geolocation-formatter', module)
+  .addDecorator(withInfo)
+  .add('geolocation component', () => (
+    <div>
+      <h1>Sample display</h1>
+      <ShowCode title={`geolocation-formmater: value is null, there will be return nothing to show.`}>
+        <GeolocationFormatter value={value1_1} />
+      </ShowCode>
+      <ShowCode title={`geolocation-formmater: value is what the geolocation formatter wanted.`}>
+        <GeolocationFormatter value={value1_2} />
+      </ShowCode>
+    </div>
+  ), {info})
+  
