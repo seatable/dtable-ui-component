98d5c2c7d43303c14d04ec06242d14180b5d26ec
shanshuirenjia <978987373@qq.com>
2020-05-27 17:25:02 +0800

    add mtime formatter

---
+++ src/components/cell-formatter/mtime-formatter.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'astro-classname';
+import moment from 'moment';
+
+const propTypes = {
+  value: PropTypes.string,
+  containerClassName: PropTypes.string
+};
+
+class MTimeFormatter extends React.Component {
+
+  static defaultProps = {
+    value: '', 
+    containerClassName: '',
+  }
+
+  formatDate = (date) => {
+    return moment(date).format('YYYY-MM-DD HH:mm:ss');
+  }
+
+  render() {
+    let { value: date, containerClassName } = this.props;
+    let classname = cn('cell-formatter-container ctime-formatter', containerClassName);
+    if (date !== '') {
+      date = this.formatDate(date);
+    }
+
+    return (
+      <div className={classname}>{date}</div>
+    );
+  }
+}
+
+MTimeFormatter.propTypes = propTypes;
+
+export default MTimeFormatter;
