69a2023e91f49acf261c6565d7cde2e1401b6b44
shanshuirenjia <978987373@qq.com>
2020-06-19 18:12:40 +0800

    update test

--- stories/components/cell-editor/08-date-editor.stories.js
+++ stories/components/cell-editor/08-date-editor.stories.js
@@ -63,44 +63,45 @@ storiesOf('Editors|date-editor', module)
           />
         </ShowCode>
         <h1>Scene Two: editor permission is read-write</h1>
-        <ShowCode sub={"date editor: no default date value in the date editor"}>
+        <ShowCode sub={"date editor: no default value in the readonly editor"}>
           <DateEditor 
             value={''}
             column={column1}
             onCommit={(updated) => action('onCommit')(updated)}
             />
         </ShowCode>
-        <ShowCode sub={"date editor: a default date value in the date editor"}>
+        <ShowCode sub={"date editor: provide a default value in the readonly editor"}>
           <DateEditor 
             value={'1992-07-09'}
             column={column1}
             onCommit={(updated) => action('onCommit')(updated)}
           />
         </ShowCode>
-        <h1>Scene Three: editor permission is read-write, the data format contain 'HH:mm'</h1>
-        <ShowCode sub={"date editor: no default date value in the date editor"}>
+        <h1>Scene Three: editor permission is read-write. the lang is 'en'</h1>
+        <ShowCode sub={"date editor: no default value in the editor"}>
           <DateEditor 
             value={''}
-            column={column2}
+            column={column1}
             onCommit={(updated) => action('onCommit')(updated)}
             />
         </ShowCode>
-        <ShowCode sub={"date editor: a default date value in the date editor"}>
+        <ShowCode sub={"date editor: a defalut value in the editor"}>
           <DateEditor 
-            value={'1992-07-09 09:09'}
-            column={column2}
+            value={'1992-07-09'}
+            column={column1}
             onCommit={(updated) => action('onCommit')(updated)}
           />
         </ShowCode>
-        <h1>Scene Three: editor permission is read-write, current lang is 'zh-cn'</h1>
-        <ShowCode sub={"date editor: no default date value in the date editor"}>
+        <h1>Scene Three: editor permission is read-write. the  lang is 'zh-cn'</h1>
+        <ShowCode sub={"date editor: you can select a time to show"}>
           <DateEditor 
+            lang={'zh-cn'}
             value={''}
             column={column2}
             onCommit={(updated) => action('onCommit')(updated)}
             />
         </ShowCode>
-        <ShowCode sub={"date editor: a default date value in the date editor"}>
+        <ShowCode sub={"date editor: you can select a time to show"}>
           <DateEditor 
             lang={'zh-cn'}
             value={'1992-07-09 09:09'}
