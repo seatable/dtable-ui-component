9daf0466f7ca8777a6bf28bce4eca1aa6029223d
shanshuirenjia <978987373@qq.com>
2020-07-03 15:09:27 +0800

    update version

--- src/components/cell-formatter/index.js
+++ src/components/cell-formatter/index.js
@@ -1,4 +1,4 @@
-import '../../assets/css/cell-formatter.css';
+import '../../css/cell-formatter.css';
 
 export { default as TextFormatter } from './text-formatter';
 export { default as NumberFormatter } from './number-formatter';
