daa7041881c6a7df28206c80db4c89ba2d2237fd
Love-YGX-and-D <1578236889@qq.com>
2021-10-09 12:01:08 +0800

    feat: update geolocation formatter

--- src/components/cell-formatter/geolocation-formatter.js
+++ src/components/cell-formatter/geolocation-formatter.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'astro-classname';
+import { getGeolocationDisplayString } from '../../utils/value-format-utils';
 
 const propTypes = {
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  data: PropTypes.object,
   containerClassName: PropTypes.string,
 };
 
@@ -16,20 +18,21 @@ class GeolocationFormatter extends React.Component {
       district: '',
       detail: '',
     },
+    data: {},
     containerClassName: ''
   }
 
   render() {
-    let { value, containerClassName } = this.props;
-    let classname = cn('dtable-ui cell-formatter-container geolocation-formatter', containerClassName);
+    let { value, data, containerClassName } = this.props;
+    let className = cn('dtable-ui cell-formatter-container geolocation-formatter', containerClassName);
 
     if (typeof value !== 'object') {
       return null;
     }
 
     return (
-      <div className={classname}>
-        {`${value.province || ''} ${value.city || ''} ${value.district || ''} ${value.detail || ''}`}
+      <div className={className}>
+        {getGeolocationDisplayString(value, data)}
       </div>
     );
   }
