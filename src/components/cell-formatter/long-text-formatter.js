add1b8f88ac5d948605d5e54757df513301ef4d7
Love-YGX-and-D <18335219360@163.com>
2021-11-16 15:11:28 +0800

    feat: long text html

--- src/components/cell-formatter/long-text-formatter.js
+++ src/components/cell-formatter/long-text-formatter.js
@@ -1,89 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'astro-classname';
-import getPreviewContent from '../../utils/normalize-long-text-value';
+import HtmlLongTextFormatter from './widgets/long-text-formatter/html-long-text-formatter';
+import SimpleLongTextFormatter from './widgets/long-text-formatter/simple-long-text-formatter';
 
-const propTypes = {
+function LongTextFormatter(props) {
+  const { isSample, value, containerClassName } = props;
+  const className= cn('dtable-ui cell-formatter-container long-text-formatter', containerClassName);
+  if (isSample) {
+    return (
+      <SimpleLongTextFormatter
+        value={value}
+        className={className}
+      />
+    );
+  }
+  return (
+    <HtmlLongTextFormatter
+      value={value}
+      className={className}
+    />
+  );
+}
+
+LongTextFormatter.propTypes = {
+  isSample: PropTypes.bool,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   containerClassName: PropTypes.string,
 };
 
-class LongTextFormatter extends React.Component {
-
-  static defaultProps = {
-    value: {
-      text: '',
-      images: [],
-      links: [],
-      preview: '',
-    },
-  }
-
-  renderLinks = (value) => {
-    const links = value.links;
-    if (!Array.isArray(links) || links.length === 0) return null;
-    return (
-      <span className="long-text-links">
-        <i className="dtable-font dtable-icon-url"></i>
-        {links.length}
-      </span>
-    );
-  }
-
-  renderCheckList = (value) => {
-    const checkList = value.checklist;
-    if (!checkList || checkList.total === 0) return null;
-    return (
-      <span className="long-text-check-list">
-        <i className={`dtable-font dtable-icon-check-square-solid ${checkList.completed === checkList.total ? 'long-text-check-list-completed' : ''}`}></i>
-        {`${checkList.completed}/${checkList.total}`}
-      </span>
-    );
-  }
-
-  renderImages = (value) => {
-    const images = value.images;
-    if (!Array.isArray(images) || images.length === 0) return null;
-    return (
-      <span className="long-text-images">
-        <img src={images[0]} alt=""/>
-        <i className="image-number">{images.length > 1 ? '+' + images.length : null}</i>
-      </span>
-    );
-  }
-
-  renderContent = (value) => {
-    return (<span className="long-text-content">{value.preview}</span>)
-  }
-
-  translateValue = () => {
-    const { value } = this.props;
-    if (!value) return {};
-    const valueType = Object.prototype.toString.call(value);
-    if (valueType === '[object String]') {
-      return getPreviewContent(value);
-    }
-    if (valueType === '[object Object]') {
-      return value;
-    }
-    return {};
-  }
-
-  render() {
-    const { containerClassName } = this.props;
-    const className = cn('dtable-ui cell-formatter-container long-text-formatter', containerClassName);
-    const value = this.translateValue();
-    return (
-      <div className={className}>
-        {this.renderLinks(value)}
-        {this.renderCheckList(value)}
-        {this.renderImages(value)}
-        {this.renderContent(value)}
-      </div>
-    );
-  }
-}
-
-LongTextFormatter.propTypes = propTypes;
+LongTextFormatter.defaultProps = {
+  isSample: true,
+};
 
 export default LongTextFormatter;
--- src/utils/normalize-long-text-value.js
+++ src/utils/normalize-long-text-value.js
@@ -1,53 +1,71 @@
-const hrefReg = /\[.+\]\(\S+\)|<img src=(\S+).+\/>|!\[\]\(\S+\)|<\S+>/g,
-  imageReg1 = /^<img src="(\S+)" .+\/>/,
-  imageReg2 = /^!\[\]\((\S+)\)/,
-  linkReg1 = /^\[.+\]\(\S+\)/,
-  linkReg2 = /^<\S+>$/;
+import { Node } from 'slate';
 
-const getLinks = (hrefList) => {
-  const hrefObj = {
-    links: [],
-    images: []
-  };
-  hrefList.forEach((href) => {
-    if (href.search(linkReg1) >= 0 || href.search(linkReg2) >= 0) {
-      hrefObj.links.push(href);
-    } else {
-      let imageSrcList = href.match(imageReg1);
-      let imageSrcList1 = href.match(imageReg2);
-      if (imageSrcList) {
-        hrefObj.images.push(imageSrcList[1]);
-      } else if (imageSrcList1) {
-        hrefObj.images.push(imageSrcList1[1]);
-      }
-    }
-  });
-  return hrefObj;
+const getPreviewContent = (content) => {
+  let previewContent = {previewText: '', images: [], links: [], checklist: {total: 0, completed: 0}};
+  getPreviewInfo(content, previewContent);
+  getPreviewText(content, previewContent);
+  return previewContent;
 };
 
-const getPreviewContent = (markdownContent) => {
-  let preview = '';
-  let newMarkdownContent = markdownContent.replace(hrefReg, '');
-  for (let index = 0; index < newMarkdownContent.length; index++) {
-    if (newMarkdownContent[index] === '#') {
-      continue;
-    } else if (newMarkdownContent[index] === '\n') {
-      preview += ' ';
+const getPreviewInfo = (nodes, previewContent) => {
+  let nodeIndex = 0;
+  while(nodes && nodeIndex <= nodes.length - 1) {
+    const currentNode = nodes[nodeIndex];
+    if (currentNode.type === 'link') {
+      previewContent.links.push(currentNode.data.href);
+    } else if (currentNode.type === 'image') {
+      previewContent.images.push(currentNode.data.src);
+    } else if (currentNode.type === 'list_item' && currentNode.data) {
+      const data = currentNode.data;
+      if (data.hasOwnProperty('checked')) {
+        previewContent.checklist.total += 1;
+        if (data.checked) {
+          previewContent.checklist.completed++;
+        }
+      }
+      getPreviewInfo(currentNode.children, previewContent);
     } else {
-      preview += newMarkdownContent[index];
+      getPreviewInfo(currentNode.children, previewContent);
     }
-    if (preview.length === 30) {
+    nodeIndex++;
+  }
+};
+
+const getTextOfNode = (node) => {
+  let text = '';
+  for(let index = 0; index < node.children.length; index++) {
+    const currentNode = node.children[index];
+    const data = currentNode.data;
+    const type = currentNode.type;
+    if (type === 'link') {
+      text += '';
+    } else if (type === 'list_item') {
+      if (data && data.hasOwnProperty('checked')) {
+        text += '';
+      } else {
+        text += Node.text(currentNode) + ' ';
+      }
+    } else {
+      if (currentNode.hasOwnProperty('text')) {
+        text += currentNode.text;
+      } else {
+        text += Node.text(currentNode) + ' ';
+      }
+    }    
+  }
+  return text;
+};
+
+const getPreviewText = (content, previewContent) => {
+  let previewText = '';
+  for(let index = 0; index < content.length; index++) {
+    previewText += getTextOfNode(content[index]) + ' ';
+    let textLength = previewText.length;
+    if (textLength >= 150) {
       break;
     }
   }
-
-  const hrefList = markdownContent.match(hrefReg);
-  if (hrefList) {
-    const { images, links } = getLinks(hrefList);
-    return { preview, images, links };
-  }
-  return { preview, images: [], links: [] };
+  previewContent.previewText = previewText;
 };
-
 
 export default getPreviewContent;
