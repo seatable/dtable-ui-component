7a6cfb04b69b35d4be230b666ae8fab3d5142516
杨国璇 <37972689+er-pai-r@users.noreply.github.com>
2022-12-23 15:09:14 +0800

    fix: style

--- src/DTableRadio/index.js
+++ src/DTableRadio/index.js
@@ -5,22 +5,30 @@ import classnames from 'classnames';
 import './index.css';
 
 function DTableRadio(props) {
+  const { className } = props;
+
   return (
-    <label className={classnames('dtable-radio', { 'dtable-radio-disable': props.disabled })}>
+    <label
+      className={classnames('dtable-radio w-100 align-items-center', {
+        'dtable-radio-disable': props.disabled,
+        [className]: className
+      })}
+    >
       <input
         type="radio"
-        className="dtable-radio-input"
+        className="dtable-radio-input position-absolute"
         checked={props.isChecked}
         onChange={props.disabled ? () => {} : props.onCheckedChange}
         name={props.name}
         value={props.value}
       />
       <span
-        className={classnames('dtable-radio-indicator', {
+        className={classnames('dtable-radio-indicator position-relative', {
           'dtable-radio-selected-indicator': props.isChecked,
+          'dtable-radio-indicator-disable': props.disabled
         })}
       ></span>
-      <span className="dtable-radio-description text-truncate">{props.label}</span>
+      <span className="dtable-radio-description text-truncate ml-2">{props.label}</span>
     </label>
   );
 }
@@ -31,6 +39,7 @@ DTableRadio.propTypes = {
   disabled: PropTypes.bool,
   name: PropTypes.string,
   value: PropTypes.any,
+  className: PropTypes.string,
   onCheckedChange: PropTypes.func,
 };
 
