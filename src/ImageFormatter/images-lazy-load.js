31bfdb69e02d7841faa50f0f229dc93239ed4068
Lewis <3421079018@qq.com>
2023-06-12 14:09:41 +0800

    Optimise image lazy load (#125)

--- src/ImageFormatter/images-lazy-load.js
+++ src/ImageFormatter/images-lazy-load.js
@@ -52,12 +52,14 @@ class ImagesLazyLoad extends React.Component {
           url,
           (image) => {
             let { loadedCount, loadedImages } = this.state;
+            if (loadedCount + 1 > images.length) return;
             let newImageList = loadedImages.slice(0);
             newImageList[index] = image;
             this.setState({ loadedCount: loadedCount + 1, loadedImages: newImageList });
           },
           () => {
             let { loadedCount } = this.state;
+            if (loadedCount + 1 > images.length) return;
             this.setState({ loadedCount: loadedCount + 1 });
           }
         );
