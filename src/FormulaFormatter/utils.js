978ebd84995557f6dd60ee1c31bdc540bca7c977
Michael An <2331806369@qq.com>
2023-06-29 12:06:11 +0800

    change formula formatter

--- src/FormulaFormatter/utils.js
+++ src/FormulaFormatter/utils.js
@@ -1,8 +1,8 @@
 import { ARRAY_FORMAL_COLUMNS, SIMPLE_CELL_COLUMNS, SIMPLE_CELL_FORMULA_RESULTS } from '../constants';
+import getPreviewContent from '../SimpleLongTextFormatter/normalize-long-text-value';
 
 export function isSimpleCellFormatter(type) {
-  return SIMPLE_CELL_COLUMNS.includes(type) ||
-    SIMPLE_CELL_FORMULA_RESULTS.includes(type);
+  return SIMPLE_CELL_COLUMNS.includes(type) || SIMPLE_CELL_FORMULA_RESULTS.includes(type);
 }
 
 export function isArrayFormalColumn(columnType) {
@@ -12,4 +12,78 @@ export function isArrayFormalColumn(columnType) {
 export const isFunction = (functionToCheck) => {
   const getType = {};
   return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+};
+
+export const isValidCellValue = (value) => {
+  if (value === undefined) return false;
+  if (value === null) return false;
+  if (value === '') return false;
+  if (JSON.stringify(value) === '{}') return false;
+  if (JSON.stringify(value) === '[]') return false;
+  return true;
+};
+
+export const getFormulaArrayValue = (value, isFlat = true) => {
+  if (!Array.isArray(value)) {
+    return [];
+  }
+  if (!isFlat) {
+    return getTwoDimensionArrayValue(value);
+  }
+  return value.map(item => {
+    if (Object.prototype.toString.call(item) !== '[object Object]' || !Object.prototype.hasOwnProperty.call(item, 'display_value')) {
+      return item;
+    }
+    const { display_value } = item;
+    if (!Array.isArray(display_value) || display_value.length === 0) {
+      return display_value;
+    }
+    return display_value.map(i => {
+      if (Object.prototype.toString.call(i) === '[object Object]') {
+        if (!Object.prototype.hasOwnProperty.call(i, 'display_value')) {
+          return i;
+        }
+        const { display_value } = i;
+        return display_value;
+      }
+      return i;
+    });
+  }).flat().filter(item => isValidCellValue(item));
+};
+
+export const getTwoDimensionArrayValue = (value) => {
+  if (!Array.isArray(value)) return [];
+  return value.map(item => {
+    if (Object.prototype.toString.call(item) !== '[object Object]') {
+      return item;
+    }
+    if (!Object.prototype.hasOwnProperty.call(item, 'display_value')) {
+      return item;
+    }
+    const { display_value } = item;
+    if (!Array.isArray(display_value) || display_value.length === 0) {
+      return display_value;
+    }
+    return display_value.map(i => {
+      if (Object.prototype.toString.call(i) === '[object Object]') {
+        if (!Object.prototype.hasOwnProperty.call(i, 'display_value')) {
+          return i;
+        }
+        const { display_value } = i;
+        return display_value;
+      }
+      return i;
+    });
+  });
+};
+
+export const convertValueToDtableLongTextValue = (value) => {
+  const valueType = Object.prototype.toString.call(value);
+  if (value && valueType === '[object String]') {
+    return getPreviewContent(value);
+  }
+  if (valueType === '[object Object]') {
+    return value;
+  }
+  return '';
 };
