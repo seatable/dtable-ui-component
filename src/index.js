ad422077d6af11ddd77decb8037c2249d11f69fd
Michael An <2331806369@qq.com>
2023-09-01 14:24:42 +0800

    add notification popover

--- src/index.js
+++ src/index.js
@@ -58,6 +58,7 @@ export { default as LinkEditor } from './LinkEditor';
 
 // dtable custom
 export { default as DTablePopover } from './DTablePopover';
+export { default as NotificationPopover } from './NotificationPopover';
 export { default as DTableSelect } from './DTableSelect';
 export { default as DTableSwitch } from './DTableSwitch';
 export { default as DTableCustomizeSelect } from './DTableCustomizeSelect';
