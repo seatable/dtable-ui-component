90fa0c247d5f7550c0bcf6e379bdbf317b802759
Michael An <2331806369@qq.com>
2024-04-15 11:19:46 +0800

    fix formular column array-type is not correct bug

--- src/ImageFormatter/utils.js
+++ src/ImageFormatter/utils.js
@@ -1,4 +1,5 @@
 export const getImageThumbnailUrl = (url, server) => {
+  if (typeof url !== 'string') return '';
   if (server && url.indexOf(server) > -1) {
     return url.replace('/workspace', '/thumbnail/workspace') + '?size=256';
   }
--- src/RowExpandImageFormatter/utils.js
+++
@@ -1,6 +0,0 @@
-export const getImageThumbnailUrl = (url, server) => {
-  if (server && url.indexOf(server) > -1) {
-    return url.replace('/workspace', '/thumbnail/workspace') + '?size=256';
-  }
-  return url;
-};
