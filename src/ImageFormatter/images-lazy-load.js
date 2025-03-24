eb554969993a3017c32aabd382a2624fffec80c1
Lewis <3421079018@qq.com>
2025-03-24 14:36:07 +0800

    Refactor images lazy load. (#302)

--- src/ImageFormatter/images-lazy-load.js
+++ src/ImageFormatter/images-lazy-load.js
@@ -1,134 +1,123 @@
-import React, { Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Loading from '../Loading';
 import { getImageThumbnailUrl } from '../utils/url';
 
-const propTypes = {
+const ImagesLazyLoad = ({ images, server, onImageClick, renderItem, dtableUuid }) => {
+  const [currentImages, setCurrentImages] = useState(images);
+  const [loadedImages, setLoadedImages] = useState([]);
+  const [loadedCount, setLoadedCount] = useState(0);
+
+  const lazyLoadImage = (url, resolve, reject) => {
+    if (!url) {
+      reject('img path is required');
+      return;
+    }
+    const image = new Image();
+    image.onload = () => { resolve(image); };
+    image.onerror = (e) => { reject(e); };
+    image.src = url;
+  };
+
+  const lazyLoadImages = () => {
+    if (!Array.isArray(currentImages) || currentImages.length === 0) {
+      return;
+    }
+
+    // Reset state for new images
+    setLoadedImages([]);
+    setLoadedCount(0);
+
+    currentImages.forEach((item, index) => {
+      const url = getImageThumbnailUrl(item, { server, dtableUuid });
+      lazyLoadImage(
+        url,
+        (image) => {
+          setLoadedImages(prev => {
+            const newList = [...prev];
+            newList[index] = image;
+            return newList;
+          });
+          setLoadedCount(count => count + 1);
+        },
+        () => {
+          setLoadedCount(count => count + 1);
+        }
+      );
+    });
+  };
+
+  useEffect(() => {
+    lazyLoadImages();
+  }, []);
+
+  useEffect(() => {
+    if (images.toString() !== currentImages.toString()) {
+      setCurrentImages(images);
+    }
+  }, [images]);
+
+  useEffect(() => {
+    if (currentImages !== images) {
+      lazyLoadImages();
+    }
+  }, [currentImages]);
+
+  const handleMouseDown = (event) => {
+    event.stopPropagation();
+  };
+
+  const handleImageClick = (event, index) => {
+    event.stopPropagation();
+    if (onImageClick) {
+      onImageClick(index);
+    }
+  };
+
+  if (!Array.isArray(currentImages) || currentImages.length === 0) {
+    return null;
+  }
+
+  if (currentImages.length > loadedCount) {
+    const style = { marginLeft: '4px' };
+    return <div style={style} className="d-flex align-items-center"><Loading /></div>;
+  }
+
+  return (
+    loadedImages.map((image, index) => {
+      let imageName = '';
+      const imageSrc = image?.src;
+
+      try {
+        imageName = imageSrc ? decodeURI(imageSrc.slice(imageSrc.lastIndexOf('/') + 1)) : '';
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      }
+
+      const imgDom = (
+        <img
+          title={imageName}
+          className="image-item"
+          src={image?.src}
+          onMouseDown={handleMouseDown}
+          onClick={(event) => handleImageClick(event, index)}
+          alt=""
+        />
+      );
+      
+      if (renderItem) return (<Fragment key={index}>{renderItem(imgDom)}</Fragment>);
+      return (<Fragment key={index}>{imgDom}</Fragment>);
+    })
+  );
+};
+
+ImagesLazyLoad.propTypes = {
   images: PropTypes.array.isRequired,
   server: PropTypes.string,
   onImageClick: PropTypes.func,
   renderItem: PropTypes.func,
   dtableUuid: PropTypes.string,
 };
-
-class ImagesLazyLoad extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      images: props.images,
-      loadedImages: [],
-      loadedCount: 0,
-    };
-  }
-
-  componentDidMount = () => {
-    this.lazyLoadImages(this.props);
-  };
-
-  UNSAFE_componentWillReceiveProps = (nextProps) => {
-    if (nextProps.images.toString() !== this.props.images.toString()) {
-      this.lazyLoadImages(nextProps);
-    }
-  };
-
-  componentWillUnmount = () => {
-    // prevent async operation
-    this.setState = (state, callback) => { return; };
-  };
-
-  lazyLoadImages = (props) => {
-    const { images } = props;
-    if (!Array.isArray(images) || images.length === 0) {
-      return;
-    }
-    this.setState({
-      images,
-      loadedImages: [],
-      loadedCount: 0
-    }, () => {
-      let { server, dtableUuid } = this.props;
-      images.forEach((item, index) => {
-        let url = getImageThumbnailUrl(item, { server, dtableUuid });
-        this.lazyLoadImage(
-          url,
-          (image) => {
-            let { loadedCount, loadedImages } = this.state;
-            let newImageList = loadedImages.slice(0);
-            newImageList[index] = image;
-            this.setState({ loadedCount: loadedCount + 1, loadedImages: newImageList });
-          },
-          () => {
-            let { loadedCount } = this.state;
-            this.setState({ loadedCount: loadedCount + 1 });
-          }
-        );
-      });
-    });
-  };
-
-  lazyLoadImage = (url, resolve, reject) => {
-    if (!url) {
-      reject('img path is require');
-      return;
-    }
-    const image = new Image();
-    image.onload = () => { resolve(image); };
-    image.onerror = e => { reject(e); };
-    image.src = url;
-  };
-
-  onMouseDown = (event) => {
-    event.stopPropagation();
-  };
-
-  onImageClick = (event, index) => {
-    event.stopPropagation();
-    this.props.onImageClick(index);
-  };
-
-  render() {
-    const { images, loadedImages, loadedCount } = this.state;
-    const { renderItem } = this.props;
-
-    if (!Array.isArray(images) || images.length === 0) {
-      return '';
-    }
-
-    if (images.length > loadedCount) {
-      let style = { marginLeft: '4px' };
-      return <div style={style} className="d-flex align-items-center"><Loading /></div>;
-    }
-
-    return (
-      loadedImages.map((image, index) => {
-        let imageName = '';
-        const imageSrc = image?.src;
-
-        try {
-          imageName = imageSrc ? decodeURI(imageSrc.slice(imageSrc.lastIndexOf('/') + 1)) : '';
-        } catch (error) {
-          // eslint-disable-next-line no-console
-          console.log(error);
-        }
-
-        const imgDom = (
-          <img
-            title={imageName}
-            className="image-item"
-            src={image.src}
-            onMouseDown={this.onMouseDown}
-            onClick={(event) => this.onImageClick(event, index)}
-            alt=""
-          />
-        );
-        if (renderItem) return (<Fragment key={index}>{renderItem(imgDom)}</Fragment>);
-        return (<Fragment key={index}>{imgDom}</Fragment>);
-      })
-    );
-  }
-}
-
-ImagesLazyLoad.propTypes = propTypes;
 
 export default ImagesLazyLoad;
