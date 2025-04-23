e0e9c662edaa066db5c9024b27a6dbd95f12064b
sailing_boat <83505853+SailingBoat762@users.noreply.github.com>
2025-04-23 16:34:29 +0800

    feat: file formatter support open file (#323)

--- src/FileFormatter/index.js
+++ src/FileFormatter/index.js
@@ -1,51 +1,126 @@
-import React, { Fragment } from 'react';
+import React, { useState, useCallback, useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import FileItemFormatter from '../FileItemFormatter';
+import ImagePreviewerLightbox from '../ImagePreviewerLightbox';
+import { imageCheck } from '../utils/url';
 
 import './index.css';
 
-export default class FileFormatter extends React.PureComponent {
+const FileFormatter = ({
+  isSample = false,
+  value = [],
+  containerClassName = '',
+  renderItem,
+  isSupportOpenFile = false,
+  server,
+  workspaceID,
+  dtableUuid,
+}) => {
+  const [largeImageIndex, setLargeImageIndex] = useState(-1);
+  const [fileImageUrlList, setFileImageUrlList] = useState([]);
 
-  static propTypes = {
-    isSample: PropTypes.bool,
-    value: PropTypes.array,
-    containerClassName: PropTypes.string,
-    renderItem: PropTypes.func,
-  };
+  const getFileItemImageUrlList = useCallback((value) => {
+    let newFileImageUrlList = [];
+    value.forEach(fileItem => {
+      const { url, name } = fileItem;
+      let assetFileIndex = typeof url === 'string' ? url.indexOf('/asset') : -1;
+      if (assetFileIndex > -1) {
+        const isImage = imageCheck(name);
+        if (isImage) {
+          newFileImageUrlList.push(url);
+        }
+      }
+    });
+    setFileImageUrlList(newFileImageUrlList);
+  }, []);
 
-  static defaultProps = {
-    isSample: false,
-    value: [],
-    containerClassName: ''
-  };
+  useEffect(() => {
+    if (!isSupportOpenFile) return;
+    getFileItemImageUrlList(value);
+  }, [value, getFileItemImageUrlList, isSupportOpenFile]);
 
-  render() {
-    let { isSample, value, containerClassName, renderItem } = this.props;
-    if (!Array.isArray(value) || value.length === 0) {
-      return null;
+  const showLargeImage = useCallback((imageIndex) => {
+    setLargeImageIndex(imageIndex);
+  }, []);
+
+  const openFile = useCallback((fileItem) => {
+    if (!isSupportOpenFile) return;
+    let openFileUrl = fileItem.url;
+    const imageIndex = fileImageUrlList.indexOf(openFileUrl);
+    if (imageIndex > -1) {
+      showLargeImage(imageIndex);
+      return;
     }
-
-    let className = classnames('dtable-ui cell-formatter-container file-formatter', containerClassName);
-
-    if (isSample) {
-      let item = value[0];
-      return (
-        <div className={className}>
-          <FileItemFormatter file={item}/>
-          {value.length !== 1 && <span className="file-item-count">{`+${value.length}`}</span>}
-        </div>
-      );
+    let previewerUrl;
+    let assetFileIndex = openFileUrl.indexOf('/asset');
+    if (assetFileIndex > -1) {
+      previewerUrl = openFileUrl.replace('/asset', '/asset-preview');
+      window.open(previewerUrl, '_blank');
     }
+  }, [fileImageUrlList, isSupportOpenFile, showLargeImage]);
 
+  const moveNext = useCallback(() => {
+    setLargeImageIndex((prevIndex) => (prevIndex + 1) % fileImageUrlList.length);
+  }, [fileImageUrlList]);
+
+  const movePrev = useCallback(() => {
+    setLargeImageIndex((prevIndex) => (prevIndex + fileImageUrlList.length - 1) % fileImageUrlList.length);
+  }, [fileImageUrlList]);
+
+  const hideLargeImage = useCallback(() => {
+    setLargeImageIndex(-1);
+  }, []);
+
+  const className = classnames('dtable-ui cell-formatter-container file-formatter', containerClassName);
+
+  if (!Array.isArray(value) || value.length === 0) {
+    return null;
+  }
+
+  if (isSample) {
+    const item = value[0];
     return (
       <div className={className}>
-        {value.map((item, index) => {
-          const dom = (<FileItemFormatter file={item} />);
-          if (renderItem) return (<Fragment key={index}>{renderItem(dom)}</Fragment>);
-          return (<Fragment key={index}>{dom}</Fragment>);
-        })}
+        <FileItemFormatter file={item}/>
+        {value.length !== 1 && <span className="file-item-count">{`+${value.length}`}</span>}
       </div>
     );
   }
-}
+
+  return (
+    <div className={className}>
+      {value.map((item, index) => {
+        const dom = (<FileItemFormatter openFile={openFile} file={item} />);
+        if (renderItem) return (<Fragment key={index}>{renderItem(dom)}</Fragment>);
+        return (<Fragment key={index}>{dom}</Fragment>);
+      })}
+      {largeImageIndex > -1 && (
+        <ImagePreviewerLightbox
+          server={server}
+          workspaceID={workspaceID}
+          dtableUuid={dtableUuid}
+          readOnly={true}
+          imageItems={fileImageUrlList}
+          imageIndex={largeImageIndex}
+          closeImagePopup={hideLargeImage}
+          moveToPrevImage={movePrev}
+          moveToNextImage={moveNext}
+        />
+      )}
+    </div>
+  );
+};
+
+FileFormatter.propTypes = {
+  isSample: PropTypes.bool,
+  value: PropTypes.array,
+  containerClassName: PropTypes.string,
+  renderItem: PropTypes.func,
+  isSupportOpenFile: PropTypes.bool,
+  server: PropTypes.string,
+  workspaceID: PropTypes.string,
+  dtableUuid: PropTypes.string,
+};
+
+export default FileFormatter;
--- src/FileItemFormatter/index.js
+++ src/FileItemFormatter/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { getFileThumbnailInfo } from '../utils/url';
 
-const FileItemFormatter = ({ file, config }) => {
+const FileItemFormatter = ({ file, config, openFile }) => {
   const { fileIconUrl, isImage } = getFileThumbnailInfo(file, config);
   return (
-    <img className={`file-item-icon ${isImage ? 'img' : ''}`} src={fileIconUrl} alt={file?.name || ''} />
+    <img onClick={() => openFile && openFile(file)} className={`file-item-icon ${isImage ? 'img' : ''}`} src={fileIconUrl} alt={file?.name || ''} />
   );
 };
 
 FileItemFormatter.propTypes = {
   file: PropTypes.object.isRequired,
   config: PropTypes.object,
+  openFile: PropTypes.func,
 };
 
 export default FileItemFormatter;
