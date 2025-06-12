6a4e562d2edb2f80e54c5f1dd8c9f28666b009e0
GongFlying <150640661+gzcqqqqqqqq1@users.noreply.github.com>
2025-06-12 10:02:32 +0800

    fix logic

--- src/DateEditor/mb-editor/index.js
+++ src/DateEditor/mb-editor/index.js
@@ -146,9 +146,7 @@ class MBDateEditor extends React.PureComponent {
           <div className="dtable-ui-mobile-date-editor-input">
             <div className="date-input" style={showHourAndMinute ? { width: '50%' } : { width: '100%' }}>
               <DatePicker mode="date" minDate={minDate} maxDate={maxDate} locale={initDateEditorLanguage(lang)} value={null} onChange={this.handleDateChange}>
-                {/* <div className="date-input-day">{dayjs(time).format(this.leftFormat)}</div> */}
-                <div className="date-input-day">{'gzc'}</div>
-
+                <div className="date-input-day">{dayjs(time).format(this.leftFormat)}</div>
               </DatePicker>
             </div>
             {showHourAndMinute &&
