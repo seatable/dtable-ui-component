89d1586372a5bc7b3f1687609cc65b19030216f8
renjie-run <rj.aiyayao@gmail.com>
2025-07-30 11:00:50 +0800

    feat: AsyncUserSelect

---
+++ src/AsyncUserSelect/user-item.js
@@ -0,0 +1,40 @@
+import { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { getLocale } from '../lang';
+
+const UserItem = ({ className, user, deleteUser, enableShowIDInOrgWhenSearchUser = false }) => {
+  const name = useMemo(() => user?.name || '', [user]);
+  const avatarUrl = useMemo(() => user?.avatar_url || '', [user]);
+  const idInOrg = useMemo(() => user?.id_in_org || '', [user]);
+
+  const onDeleteUser = (event) => {
+    event.stopPropagation();
+    event && event.nativeEvent.stopImmediatePropagation();
+    deleteUser(user);
+  };
+
+  return (
+    <div className={classnames('dtable-ui-user-item', className)} title={name} aria-label={name}>
+      <span className="user-avatar">
+        <img className="user-avatar-icon" alt={name} src={avatarUrl} />
+      </span>
+      <span className="user-name text-truncate">{name}</span>
+      {(enableShowIDInOrgWhenSearchUser && !deleteUser) && <span className="user-id-in-org text-truncate">({idInOrg})</span>}
+      {deleteUser && (
+        <span className="user-remove ml-2" onClick={onDeleteUser} title={getLocale('Remove')}>
+          <i className="dtable-font dtable-icon-x" aria-hidden="true"></i>
+        </span>
+      )}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  className: PropTypes.string,
+  user: PropTypes.object,
+  enableShowIDInOrgWhenSearchUser: PropTypes.bool,
+  deleteUser: PropTypes.func,
+};
+
+export default UserItem;
