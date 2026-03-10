aed20b0ca7518acc4b7ffd9e9fb764029fd0f021
张玉鑫 <zhangyuxin9102@163.com>
2026-03-10 18:18:06 +0800

    update

--- src/DTableIcon/svg-icon.jsx
+++ src/DTableIcon/svg-icon.jsx
@@ -24,7 +24,7 @@ const getSvgIconComponents = () => {
 const SvgIcon = ({ className, symbol, color, ariaHidden, getSvg }) => {
   if (!symbol) return null;
 
-  let props = { style: { color: color } };
+  let props = { style: { color } };
   if (ariaHidden) {
     props['aria-hidden'] = ariaHidden;
   }
