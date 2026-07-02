f7a97d6f3ac9477f4173d24c97b49db841727d4b
GongFlying <150640661+gzcqqqqqqqq1@users.noreply.github.com>
2026-07-02 16:00:23 +0800

    update ui

--- src/DTableSelect/utils.js
+++ src/DTableSelect/utils.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { components } from 'react-select';
-
+import './dtable-select-label.css';
 // DtableSelect is based on seatable-ui.css, so use the following content to override the default react-select style
 const DEFAULT_CONTROL_STYLE = {
   fontSize: '14px',
@@ -78,7 +78,6 @@ const MenuSelectStyle = {
   menu: (base) => {
     return ({
       ...base,
-      padding: '8px',
       backgroundColor: 'var(--bs-popover-bg)',
       border: '1px solid var(--bs-border-secondary-color)',
       borderRadius: '4px',
@@ -89,7 +88,7 @@ const MenuSelectStyle = {
   },
   menuList: (provided) => ({
     ...provided,
-    padding: 0,
+    padding: '8px',
   }),
   option: (provided, state) => {
     const { isDisabled } = state;
@@ -105,7 +104,7 @@ const MenuSelectStyle = {
       whiteSpace: 'nowrap',
       textOverflow: 'ellipsis',
       ':hover': {
-        backgroundColor: 'rgba(0, 0, 0, 0.04)',
+        backgroundColor: 'var(--bs-btn-background-hover)',
       },
       '.header-icon': HEADER_ICON_STYLE });
   },
@@ -210,7 +209,7 @@ const Option = props => {
   return (
     <components.Option {...props}>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%', whiteSpace: 'pre-line' }}>
-        <span>{label}</span>
+        <div className='seatable-option'>{label}</div>
         {isSelected && <span className="dtable-font dtable-icon-check" style={{ fontSize: '14px', color: 'var(--bs-icon-color)', paddingLeft: '16px' }}></span>}
       </div>
     </components.Option>
